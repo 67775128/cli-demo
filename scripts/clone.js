e45f419665845c6fc2d518f536984c51c46a1b55
xiangdan1 <xiangdan1@vipkid.com.cn>
2019-11-18 00:16:18 +0800

    update

--- scripts/clone.js
+++ scripts/clone.js
@@ -5,17 +5,17 @@ const fs = require('fs')
 module.exports = (pkgLink) => {
     let tpldirName = pkgLink.replace(/\.git/,'')
     tpldirName = tpldirName.match(/\w+$/)[0]
-    const isTpldirsExist = fs.existsSync(path.join('./tpls', tpldirName))
-    console.log(isTpldirsExist)
+    const tplPath = path.join(__dirname, '../tpls/'+tpldirName)
+    const isTpldirsExist = fs.existsSync(tplPath)
+    console.log(tplPath, isTpldirsExist)
+    console.log(process.cwd())
     if (isTpldirsExist) {
         console.log(chalk.yellow(`开始删除文件夹：${tpldirName}`))
-        execSync(`rm -rf ${path.join('./tpls', tpldirName)}`)
+        execSync(`rm -rf ${tplPath}`)
         console.log(chalk.green(`删除文件夹：${tpldirName}完成！`))
     }
-    process.chdir('./tpls')
     console.log(chalk.yellow('开始template下载！'))
-    execSync(`git clone ${pkgLink}`)
-    process.chdir('../')
+    execSync(`git clone ${pkgLink} ${tplPath}`)
     console.log(process.cwd())
     console.log(chalk.green('template下载完成！'))
 }
--- scripts/packageSet.js
+++ scripts/packageSet.js
@@ -2,7 +2,14 @@ const Generator = require('yeoman-generator')
 const yoemanEnv = require('yeoman-environment').createEnv()
 const fs = require('fs')
 const path = require('path')
+var pkgLink = null
 const myGenerator = class extends Generator {
+  constructor (args, opts) {
+      super(args, opts);
+      let tpldirName = opts.pkgLink.replace(/\.git/,'')
+      tpldirName = tpldirName.match(/\w+$/)[0]
+      this.tpldirName = tpldirName
+  }
   prompting() {
     return this.prompt([
       {
@@ -11,7 +18,7 @@ const myGenerator = class extends Generator {
         message: '项目名称：',
         validate(input) {
           if (!input) return '请输入项目名称'
-          if (fs.readdirSync('./createdTpls').includes(input)) {
+          if (fs.readdirSync(process.cwd()).includes(input)) {
             return '目录已存在'
           }
           return true
@@ -30,9 +37,11 @@ const myGenerator = class extends Generator {
   }
   writing() {
     const { language, appName } = this.answers
-    console.log(process.cwd(), this.templatePath(), this.destinationPath())
+      console.log(process.cwd(), this.tpldirName, this.templatePath(), this.destinationPath())
     // copyTpl 会使用模板引擎，替换 <%= appName %>
-    // this.fs.copyTpl(this.templatePath(language, '_package.json'), this.destinationPath(appName, 'package.json'), this.answers)
+      const tplPath = path.join(__dirname, '../tpls/'+this.tpldirName)
+
+      this.fs.copyTpl(tplPath, process.cwd()+'/'+appName, this.answers)
     // copy 支持文件/文件夹
     // fs.readdirSync(this.templatePath(language))
     //   .filter(name => !name.startsWith('_'))
@@ -49,7 +58,8 @@ const myGenerator = class extends Generator {
     this.log('happy coding!')
   }
 }
-module.exports = function () {
+module.exports = function (pkgLink) {
+  pkgLink = pkgLink
   yoemanEnv.registerStub(myGenerator, 'test')
-  yoemanEnv.run('test')
+  yoemanEnv.run('test', {pkgLink})
 }
