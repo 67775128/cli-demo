7ad2dd7acd7f3c6dfdf9f237fa8b1576e1dd8abd
xiangdan1 <xiangdan1@vipkid.com.cn>
2019-11-19 15:27:21 +0800

    update

--- scripts/clone.js
+++ scripts/clone.js
@@ -2,6 +2,8 @@ const execSync = require('child_process').execSync
 const chalk = require('chalk')
 const path = require('path')
 const fs = require('fs')
+const ora = require('ora')
+
 module.exports = (pkgLink) => {
     let tpldirName = pkgLink.replace(/\.git/,'')
     tpldirName = tpldirName.match(/\w+$/)[0]
@@ -15,7 +17,21 @@ module.exports = (pkgLink) => {
         console.log(chalk.green(`删除文件夹：${tpldirName}完成！`))
     }
     console.log(chalk.yellow('开始template下载！'))
-    execSync(`git clone ${pkgLink} ${tplPath}`)
+    const  oraInstance = ora({
+        color: 'red',
+        text: 'loading...'
+    }).start()
+    const clildprocess = execSync(`git clone ${pkgLink} ${tplPath} --progress`)
+    // clildprocess.stdout.on('data', (m) => {
+    //     console.log('stdout', m);
+    // });
+    // clildprocess.stderr.on('data', (m) => {
+    //     console.log('stderr', m);
+    // });
+    // clildprocess.on('message', (m) => {
+    //     console.log('message', m);
+    // });
     console.log(process.cwd())
+    oraInstance.succeed('template下载完成！')
     console.log(chalk.green('template下载完成！'))
 }
